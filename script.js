448ed82e75785def095a100b4ffcb7f91f9524cb
Vitor Costa <107156481+vitor167pk@users.noreply.github.com>
2023-06-12 14:21:43 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+// Funções de exemplo para navegação
+function goToHome() {
+    console.log('Home');
+  }
+  
+  function goToHistory() {
+    console.log('History');
+  }
+  
+  function goToSettings() {
+    console.log('Settings');
+  }
+  
+  function goToCalories() {
+    console.log('Calories');
+  }
+  
